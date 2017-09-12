ec0078fefdce6e7ae49c22942ac9600ab4f0ef49
wangjiayin <wangjiayin@baidu.com>
2017-09-12 20:55:25 +0800

    no message

---
+++ src/main/java/com/algorithm/sort/BubbleSort.java
@@ -0,0 +1,15 @@
+package com.algorithm.sort;
+
+/**
+ * <p>
+ * 冒泡排序：
+ * <p>
+ * 交换排序的一种，与一般交换排序的区别，通俗的说，在第一次循环内：
+ * 冒泡排序是1和2比，2和3比，3和4比...
+ * 一般交换排序是1和2比，1和3比，1和4比...
+ *
+ * @author wangjiayin@baidu.com
+ * @since 2017/9/12
+ */
+public class BubbleSort {
+}
---
+++ src/main/java/com/algorithm/sort/QuickSort.java
@@ -0,0 +1,14 @@
+package com.algorithm.sort;
+
+/**
+ * <p>
+ * 快速排序：
+ * <p>
+ * 每次选定一个元素，将比该元素大的元素放在所选定元素的右边，将比该元素小的放在左边。
+ * 可以利用递归方式实现。
+ *
+ * @author wangjiayin@baidu.com
+ * @since 2017/9/12
+ */
+public class QuickSort {
+}
